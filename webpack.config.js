d43575156703cb8bc2920c74169ce897f876c5f8
mariaclarabs <clarabsouza@hotmail.com>
2021-06-18 20:33:15 -0300

    CSS loader/Global Styles

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,14 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ],
   },
