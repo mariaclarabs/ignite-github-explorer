670e44cf249c209454e704e68fe87529785c4f06
mariaclarabs <clarabsouza@hotmail.com>
2021-06-18 20:38:19 -0300

    Config SASS loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,11 +30,12 @@ module.exports = {
         use: 'babel-loader',
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         exclude: /node_modules/,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          'sass-loader'
         ]
       }
     ],
